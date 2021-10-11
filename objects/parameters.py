b6d661ca6ed5f9ecb76f1e45048214e64401edcc
williamsnider <williamgraysonsnider@gmail.com>
2021-10-11 00:15:09 -0400

    WIP: Fairing the peg.

--- objects/parameters.py
+++ objects/parameters.py
@@ -8,8 +8,8 @@ SHRINK_FACTOR = 0.3  # Impacts surface slope at cross sections adjacent to endpo
 ORDER = 3  # quadratic B-spline
 
 # Shape construction
-HARMONIC_POWER = 3  # 2-> G1 curvature fairing, 3-> G2 curvature fairing, etc
-FAIRING_DISTANCE = 0.1  # How far (mm) from a junction should vertices in a union mesh be faired
+HARMONIC_POWER = 2  # 2-> G1 curvature fairing, 3-> G2 curvature fairing, etc
+FAIRING_DISTANCE = 0.2  # How far (mm) from a junction should vertices in a union mesh be faired
 
 # Interface construction
 CUBE_SIDE_LENGTH = 25  # mm
@@ -18,4 +18,7 @@ POST_HEIGHT = 15
 POST_SECTIONS = 10  # Strangely, 8 causes the interface to be non-watertight
 FINGERTIP_SLOT_SIDE_LENGTH = 5
 PEG_SIDE_LENGTH = 10  # mm
-PEG_DEPTH = 12  # mm
+PEG_CORNER_RADIUS = 2  # mm - Round the edges to fit into waterjet cut slots better
+PEG_CORNER_NUM_STEPS = 3
+PEG_DEPTH = 20  # mm
+PEG_SPHERE_SUBDIVISIONS = 3  # Higher --> more points used to fair peg tip
