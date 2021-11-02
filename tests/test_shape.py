fb5547ae53756795847a5ba19c904c478b3f5a4b
williamsnider <williamgraysonsnider@gmail.com>
2021-11-02 16:47:14 -0400

    Script for thesis committee meeting

--- tests/test_shape.py
+++ tests/test_shape.py
@@ -25,42 +25,43 @@ cp = np.array(
         [0, 10, 0],
         [0, 20, 0],
         [0, 30, 0],
-        [10, 30, 0],
-        [20, 30, 0],
-        [30, 30, 0],
+        [10, 40, 0],
+        [20, 50, 0],
+        [30, 60, 0],
     ]
 )
 backbone1 = Backbone(cp, reparameterize=False)
 
 
-def test_fuse_meshes():
+# def test_fuse_meshes():
 
-    cs0 = CrossSection(base_cp * 5, 0.3)
-    cs1 = CrossSection(base_cp * 5, 0.7)
-    ac1 = AxialComponent(backbone1, cross_sections=[cs0, cs1])
-    ac2 = AxialComponent(
-        backbone1,
-        cross_sections=[cs0, cs1],
-        parent_axial_component=ac1,
-        position_along_parent=0.2,
-        position_along_self=0.75,
-    )
-    s = Shape([ac1, ac2])
-    s.fuse_meshes()
-    s.mesh.show(smooth=False)
+#     cs0 = CrossSection(base_cp * 5, 0.3)
+#     cs1 = CrossSection(base_cp * 5, 0.7)
+#     ac1 = AxialComponent(backbone1, cross_sections=[cs0, cs1])
+#     ac2 = AxialComponent(
+#         backbone1,
+#         cross_sections=[cs0, cs1],
+#         parent_axial_component=ac1,
+#         position_along_parent=0.2,
+#         position_along_self=0.75,
+#     )
+#     s = Shape([ac1, ac2])
+#     s.fuse_meshes()
+#     s.mesh.show(smooth=False)
 
-    # Debug
-    import trimesh
+#     # Debug
+#     import trimesh
 
-    scene = trimesh.Scene([ac1.mesh, ac2.mesh])
-    scene.show()
+#     scene = trimesh.Scene([ac1.mesh, ac2.mesh])
+#     scene.show()
 
 
 def test_align_mesh():
 
-    cs0 = CrossSection(base_cp * 15, 0.3)
-    cs1 = CrossSection(base_cp * 15, 0.7)
-    ac1 = AxialComponent(backbone1, cross_sections=[cs0, cs1])
+    cs0 = CrossSection(base_cp * 20, 0.2)
+    cs1 = CrossSection(base_cp * 10, 0.5)
+    cs2 = CrossSection(base_cp * 20, 0.8)
+    ac1 = AxialComponent(backbone1, cross_sections=[cs0, cs1, cs2])
     # ac2 = AxialComponent(
     #     40 * np.pi * 1 * 0.25,
     #     curvature=1 / 20,
@@ -71,7 +72,7 @@ def test_align_mesh():
     #     euler_angles=np.array([0, np.pi / 3, 0]),
     # )
     s = Shape([ac1], align_OBB=True, fuse_to_interface=True)
-    # s.merged_meshes.show()
+    s.mesh.show()
 
 
 def test_export_stl():
