902820fce35dfb611de5401de3181bf1c8e03d4f
williamsnider <williamgraysonsnider@gmail.com>
2022-03-24 10:37:29 -0400

    Uploading ipynb

--- scripts/stimulus_set.py
+++ scripts/stimulus_set.py
@@ -111,176 +111,176 @@ for b_name in ["b_flat", "b_weak_curve", "b_strong_curve", "b_sharp_bend", "b_ho
     s.sd_name = None
     shapes.append(s)
 
-# Varying cross sections
-# #TODO: rotations?
-for b_name in ["b_flat", "b_weak_curve"]:
-    for cs_name in ["cs_round", "cs_concave", "cs_plane", "cs_convex", "cs_elliptical"]:
+# # Varying cross sections
+# # #TODO: rotations?
+# for b_name in ["b_flat", "b_weak_curve"]:
+#     for cs_name in ["cs_round", "cs_concave", "cs_plane", "cs_convex", "cs_elliptical"]:
 
-        if b_name == "b_flat" and cs_name == "cs_round":
-            continue
+#         if b_name == "b_flat" and cs_name == "cs_round":
+#             continue
 
-        backbone = d[b_name]
-        cp = d[cs_name]
-        rotation = 0
-        cs_list = [CrossSection(cp, i, rotation=rotation) for i in np.linspace(0.1, 0.9, 10)]
-        ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
-        s = Shape([ac])
-        s.description = "{0}-{1}".format(b_name, cs_name)
-        s.cs_name = [cs_name]
-        s.sd_name = None
-        shapes.append(s)
+#         backbone = d[b_name]
+#         cp = d[cs_name]
+#         rotation = 0
+#         cs_list = [CrossSection(cp, i, rotation=rotation) for i in np.linspace(0.1, 0.9, 10)]
+#         ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
+#         s = Shape([ac])
+#         s.description = "{0}-{1}".format(b_name, cs_name)
+#         s.cs_name = [cs_name]
+#         s.sd_name = None
+#         shapes.append(s)
 
-# Blending cross sections
-# #TODO: rotations?
-for cs_name_a in ["cs_round", "cs_elliptical"]:
-    for cs_name_b in ["cs_round", "cs_concave", "cs_plane", "cs_convex", "cs_elliptical"]:
+# # Blending cross sections
+# # #TODO: rotations?
+# for cs_name_a in ["cs_round", "cs_elliptical"]:
+#     for cs_name_b in ["cs_round", "cs_concave", "cs_plane", "cs_convex", "cs_elliptical"]:
 
-        # Skip when cp_a and cp_b are the same cross section, as its already formed above
-        if cs_name_a == cs_name_b:
-            continue
+#         # Skip when cp_a and cp_b are the same cross section, as its already formed above
+#         if cs_name_a == cs_name_b:
+#             continue
 
-        # Do not duplicate cs_elliptical + cs_round
-        if cs_name_a == "cs_elliptical" and cs_name_b == "cs_round":
-            continue
+#         # Do not duplicate cs_elliptical + cs_round
+#         if cs_name_a == "cs_elliptical" and cs_name_b == "cs_round":
+#             continue
 
-        b_name = "b_flat"
-        cp_a = d[cs_name_a]
-        cp_b = d[cs_name_b]
+#         b_name = "b_flat"
+#         cp_a = d[cs_name_a]
+#         cp_b = d[cs_name_b]
 
-        # Blend in both directions (i.e. shape AB)
-        backbone = d[b_name]
-        rotation = 0
-        cs_list = [CrossSection(cp_a, 0.1, rotation=rotation), CrossSection(cp_b, 0.9, rotation=rotation)]
-        ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
-        s = Shape([ac])
-        s.description = "{0}-{1}-{2}".format(b_name, cs_name_a, cs_name_b)
-        s.cs_name = [cs_name_a, cs_name_b]
-        s.sd_name = None
-        shapes.append(s)
+#         # Blend in both directions (i.e. shape AB)
+#         backbone = d[b_name]
+#         rotation = 0
+#         cs_list = [CrossSection(cp_a, 0.1, rotation=rotation), CrossSection(cp_b, 0.9, rotation=rotation)]
+#         ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
+#         s = Shape([ac])
+#         s.description = "{0}-{1}-{2}".format(b_name, cs_name_a, cs_name_b)
+#         s.cs_name = [cs_name_a, cs_name_b]
+#         s.sd_name = None
+#         shapes.append(s)
 
-        # Blend in both directions (i.e. shape BA)
-        backbone = backbone_flat
-        rotation = 0
-        cs_list = [CrossSection(cp_b, 0.1, rotation=rotation), CrossSection(cp_a, 0.9, rotation=rotation)]
-        ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
-        s = Shape([ac])
-        s.description = "{0}-{1}-{2}".format(b_name, cs_name_b, cs_name_a)
-        s.cs_name = [cs_name_b, cs_name_a]
-        s.sd_name = None
-        shapes.append(s)
+#         # Blend in both directions (i.e. shape BA)
+#         backbone = backbone_flat
+#         rotation = 0
+#         cs_list = [CrossSection(cp_b, 0.1, rotation=rotation), CrossSection(cp_a, 0.9, rotation=rotation)]
+#         ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
+#         s = Shape([ac])
+#         s.description = "{0}-{1}-{2}".format(b_name, cs_name_b, cs_name_a)
+#         s.cs_name = [cs_name_b, cs_name_a]
+#         s.sd_name = None
+#         shapes.append(s)
 
-# Varying cross section relative sizes
-sizes = np.array(
-    [
-        [0.5, 1, 1.5],  # linear increase (cone)
-        [1.5, 1, 0.5],  # linear decrease (cone)
-        [0.5, 1, 2],  # quadratic increase (flare)
-        [2, 1, 0.5],  # quadratic decrease (flare)
-        [0.5, 1.5, 0.5],  # spindle
-        [1.5, 0.5, 1.5],  # hourglass
-    ]
-)
-for size in sizes:
-    for cs_name in ["cs_round"]:
-        b_name = "b_flat"
-        backbone = d[b_name]
-        cp = d[cs_name]
-        rotation = 0
-        cs_list = [
-            CrossSection(cp * size[0], 0.1, rotation=rotation),
-            CrossSection(cp * size[1], 0.5, rotation=rotation),
-            CrossSection(cp * size[2], 0.9, rotation=rotation),
-        ]
-        ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
-        s = Shape([ac])
-        size_as_str = [str(int(s * 100)) + "%" for s in size]
-        s.description = "{0}-{1}-{2}".format(b_name, cs_name, "-".join(size_as_str))
-        s.cs_name = [cs_name]
-        s.sd_name = None
-        shapes.append(s)
+# # Varying cross section relative sizes
+# sizes = np.array(
+#     [
+#         [0.5, 1, 1.5],  # linear increase (cone)
+#         [1.5, 1, 0.5],  # linear decrease (cone)
+#         [0.5, 1, 2],  # quadratic increase (flare)
+#         [2, 1, 0.5],  # quadratic decrease (flare)
+#         [0.5, 1.5, 0.5],  # spindle
+#         [1.5, 0.5, 1.5],  # hourglass
+#     ]
+# )
+# for size in sizes:
+#     for cs_name in ["cs_round"]:
+#         b_name = "b_flat"
+#         backbone = d[b_name]
+#         cp = d[cs_name]
+#         rotation = 0
+#         cs_list = [
+#             CrossSection(cp * size[0], 0.1, rotation=rotation),
+#             CrossSection(cp * size[1], 0.5, rotation=rotation),
+#             CrossSection(cp * size[2], 0.9, rotation=rotation),
+#         ]
+#         ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
+#         s = Shape([ac])
+#         size_as_str = [str(int(s * 100)) + "%" for s in size]
+#         s.description = "{0}-{1}-{2}".format(b_name, cs_name, "-".join(size_as_str))
+#         s.cs_name = [cs_name]
+#         s.sd_name = None
+#         shapes.append(s)
 
-# Varying surface deformations
-for b_name in ["b_flat"]:
-    backbone = d[b_name]
-    for cs_name in ["cs_round"]:
+# # Varying surface deformations
+# for b_name in ["b_flat"]:
+#     backbone = d[b_name]
+#     for cs_name in ["cs_round"]:
 
-        cp = d[cs_name]
+#         cp = d[cs_name]
 
-        # Generate base shape
-        rotation = 0
-        cs_list = [CrossSection(cp, i, rotation=rotation) for i in np.linspace(0.1, 0.9, 9)]
-        ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
-        base_shape = Shape([ac])
+#         # Generate base shape
+#         rotation = 0
+#         cs_list = [CrossSection(cp, i, rotation=rotation) for i in np.linspace(0.1, 0.9, 9)]
+#         ac = AxialComponent(backbone=backbone, cross_sections=cs_list)
+#         base_shape = Shape([ac])
 
-        # Apply surface deformations
-        for sd_name in [
-            "sd_sphere",
-            "sd_ellipsoid",
-            "sd_cylinder",
-            "sd_cylinder_spherical_top",
-            "sd_curved_cylinder",
-            "sd_cone",
-        ]:
+#         # Apply surface deformations
+#         for sd_name in [
+#             "sd_sphere",
+#             "sd_ellipsoid",
+#             "sd_cylinder",
+#             "sd_cylinder_spherical_top",
+#             "sd_curved_cylinder",
+#             "sd_cone",
+#         ]:
 
-            sd = d[sd_name]
+#             sd = d[sd_name]
 
-            # Rotations around round axis of axial component
-            for theta_backbone in np.linspace(0, 2 * np.pi, 4, endpoint=False):
+#             # Rotations around round axis of axial component
+#             for theta_backbone in np.linspace(0, 2 * np.pi, 4, endpoint=False):
 
-                if theta_backbone != 0 and backbone == backbone_flat:
-                    continue
+#                 if theta_backbone != 0 and backbone == backbone_flat:
+#                     continue
 
-                # Rotations about vector from backbone to surface point where surface deformation is attached
-                for theta_linear_segment in np.linspace(0, 2 * np.pi, 4, endpoint=False):
+#                 # Rotations about vector from backbone to surface point where surface deformation is attached
+#                 for theta_linear_segment in np.linspace(0, 2 * np.pi, 4, endpoint=False):
 
-                    if ~np.isclose(theta_linear_segment, 0) and sd not in [
-                        sd_curved_cylinder,
-                        sd_ellipsoid,
-                    ]:
-                        continue
+#                     if ~np.isclose(theta_linear_segment, 0) and sd not in [
+#                         sd_curved_cylinder,
+#                         sd_ellipsoid,
+#                     ]:
+#                         continue
 
-                    # Don't duplicate the ellipsoid with rotation
-                    if sd == sd_ellipsoid:
-                        pass
-                    if np.any(np.isclose(theta_linear_segment, [np.pi, 3 * np.pi / 2])) and sd in [
-                        sd_ellipsoid,
-                    ]:
-                        continue
+#                     # Don't duplicate the ellipsoid with rotation
+#                     if sd == sd_ellipsoid:
+#                         pass
+#                     if np.any(np.isclose(theta_linear_segment, [np.pi, 3 * np.pi / 2])) and sd in [
+#                         sd_ellipsoid,
+#                     ]:
+#                         continue
 
-                    # Apply "difference" operation to make concavity
-                    for operation in ["union", "difference"]:
-                        if operation == "difference" and sd not in [sd_sphere, sd_ellipsoid]:
-                            continue
+#                     # Apply "difference" operation to make concavity
+#                     for operation in ["union", "difference"]:
+#                         if operation == "difference" and sd not in [sd_sphere, sd_ellipsoid]:
+#                             continue
 
-                        # Allow surface deformation to be in 1-2 positions
-                        for pos_list in [[0.33], [0.66], [0.33, 0.66]]:
-                            s = copy.deepcopy(base_shape)
-                            for pos in pos_list:
-                                sd_mesh, origin = sd
-                                sd_mesh_transformed = transform_sd_mesh(
-                                    sd_mesh, origin, ac, pos, theta_backbone, theta_linear_segment
-                                )
-                                # sd_mesh_rotations = [transform_sd_mesh(sd_mesh, origin, ac, pos, theta_backbone, theta_linear_segment) for theta_linear_segment in np.linspace(0, 2*np.pi, 4, endpoint=False)]
-                                s.combine_meshes([s.mesh, sd_mesh_transformed], operation=operation)
-                            # s.mesh.show()
+#                         # Allow surface deformation to be in 1-2 positions
+#                         for pos_list in [[0.33], [0.66], [0.33, 0.66]]:
+#                             s = copy.deepcopy(base_shape)
+#                             for pos in pos_list:
+#                                 sd_mesh, origin = sd
+#                                 sd_mesh_transformed = transform_sd_mesh(
+#                                     sd_mesh, origin, ac, pos, theta_backbone, theta_linear_segment
+#                                 )
+#                                 # sd_mesh_rotations = [transform_sd_mesh(sd_mesh, origin, ac, pos, theta_backbone, theta_linear_segment) for theta_linear_segment in np.linspace(0, 2*np.pi, 4, endpoint=False)]
+#                                 s.combine_meshes([s.mesh, sd_mesh_transformed], operation=operation)
+#                             # s.mesh.show()
 
-                            # b_flat-cs_round-sd_sphere-rot_b_90-rot_ls_90-pos_0.33_0.66
-                            if operation == "union":
-                                curv = "convex"
-                            elif operation == "difference":
-                                curv = "concave"
-                            s.description = "{0}-{1}-{2}-rot_b_{3}-rot_ls_{4}-pos_{5}_{6}".format(
-                                b_name,
-                                cs_name_a,
-                                sd_name,
-                                str(np.round(theta_backbone / np.pi * 180).astype("int")),
-                                str(np.round(theta_linear_segment / np.pi * 180).astype("int")),
-                                "_".join([str(p) for p in pos_list]),
-                                curv,
-                            )
-                            s.cs_name = [cs_name]
-                            s.sd_name = sd_name
-                            shapes.append(s)
+#                             # b_flat-cs_round-sd_sphere-rot_b_90-rot_ls_90-pos_0.33_0.66
+#                             if operation == "union":
+#                                 curv = "convex"
+#                             elif operation == "difference":
+#                                 curv = "concave"
+#                             s.description = "{0}-{1}-{2}-rot_b_{3}-rot_ls_{4}-pos_{5}_{6}".format(
+#                                 b_name,
+#                                 cs_name_a,
+#                                 sd_name,
+#                                 str(np.round(theta_backbone / np.pi * 180).astype("int")),
+#                                 str(np.round(theta_linear_segment / np.pi * 180).astype("int")),
+#                                 "_".join([str(p) for p in pos_list]),
+#                                 curv,
+#                             )
+#                             s.cs_name = [cs_name]
+#                             s.sd_name = sd_name
+#                             shapes.append(s)
 
 # Labeling the shapes
 for i, s in enumerate(shapes):
