be3671086838a882cde42ddfc9ca1c58cdfb63aa
William Snider <williamgraysonsnider@gmail.com>
2023-01-23 16:59:32 -0500

    WIP: Fusing interface

--- scripts/stimulus_set_A.py
+++ scripts/stimulus_set_A.py
@@ -19,7 +19,8 @@ NUM_CP_PER_CROSS_SECTION = 8
 angle = np.pi
 dist = LENGTH
 radius = dist/angle
-t = np.linspace(0,angle,50).reshape(-1,1)
+t = np.linspace(angle,0,15, endpoint=False).reshape(-1,1)
+t = np.flip(t)
 c = np.cos
 s = np.sin
 curve_cp = np.hstack([radius*s(t), radius*c(t), np.zeros(t.shape)])
@@ -63,18 +64,54 @@ th = np.linspace(0, 2*np.pi, NUM_CP_PER_CROSS_SECTION, endpoint=False).reshape(-
 cs_cp = np.hstack((cp_radius*np.cos(th), cp_radius*np.sin(th)))
 post_radius = fit_radius(5, NUM_CP_PER_CROSS_SECTION)
 post_cp = np.hstack((post_radius*np.cos(th), post_radius*np.sin(th)))
-curve_t = np.flip(np.linspace(1,1-2*(1-mid_t),10, endpoint=False))
+curve_t = np.flip(np.linspace(1,1-2*(1-mid_t),20, endpoint=False))
 cs_list_curve = [CrossSection(controlpoints=cs_cp, position=position) for position in curve_t]
-cs_list_post = [CrossSection(controlpoints=post_cp, position=position) for position in np.linspace(0,(1-2*(1-mid_t)),3, endpoint=False)]
+cs_list_post = [CrossSection(controlpoints=post_cp, position=position) for position in np.linspace(0,(1-2*(1-mid_t))*1/2,3, endpoint=True)]
 cs_list = [*cs_list_post,*cs_list_curve]
 # Axial Component
 ac = AxialComponent(backbone, cs_list, smooth_with_post=False)
 ac.mesh.show()
 
+
+post_begin = 1-2*(1-mid_t)
+new_t = np.linspace(0,1,20)
+new_cs_curve = [CrossSection(controlpoints=cs_cp, position=position) for position in new_t[new_t>=post_begin]]
+new_cs_post = [CrossSection(controlpoints=post_cp, position=position) for position in new_t[new_t<post_begin]]
+new_ac = AxialComponent(backbone, [*new_cs_post, *new_cs_curve], smooth_with_post=False)
+new_ac.mesh.show(smooth=False)
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 for i in range(ac.controlpoints.shape[0]):
     ax.plot(ac.controlpoints[i,:,0],ac.controlpoints[i,:,1],ac.controlpoints[i,:,2],"b-")
+plt.show()
+
+
+## Fuse to interface
+
+# Generate ac
+new_t = np.linspace(0,1,20)
+cs_curve = [CrossSection(controlpoints=cs_cp, position=position) for position in new_t]
+backbone_cp = approximate_arc(backbone_angle, LENGTH)
+backbone = Backbone(backbone_cp, reparameterize=True)
+ac = AxialComponent(backbone, cs_curve, smooth_with_post=False)
+ac.mesh.show(smooth=False)
+
+# Shift ac so that furthest vertex along -x axis are at origin
+old_verts = ac.mesh.vertices
+bottommost_vert = old_verts[old_verts[:,1].argmin()]
+new_verts = old_verts - bottommost_vert
+
+# Add in interface
+
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot(new_verts[:,0],new_verts[:,1], new_verts[:,2], "k.")
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
 plt.show()
 
 # plt.show()
